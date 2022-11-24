d0762275a60efd2bdc23bbd0226c19edcbd866a0
Bardane <theo.martin@efrei.net>
2022-11-24 11:22:18 +0100

    read

--- airportproject/src/main/scala/service/csvParser.scala
+++ airportproject/src/main/scala/service/csvParser.scala
@@ -1,5 +1,13 @@
 package service
 
-class csvParser {
+import java.nio.file.{Files, Path}
+import scala.jdk.CollectionConverters.IteratorHasAsScala
+
+final case class ReadResult[A](lines: Iterator[A], nbInvalidLine: Int)
+
+object csvParser {
+
+  def read[A](fileName: String, parseLine: Array[String] => Option[A], regex: String = ","): ReadResult[A] = ???
 
 }
+
